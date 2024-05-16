8f6a4ba3415f1157371a36472c3bcb17b9342067
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-16 20:27:21 +0530

    .\distructingAssigment.js

---
+++ javascript/distructingAssigment.js
@@ -0,0 +1,19 @@
+//this is one of the most popular ES6 feachers that helps to extract the arrays and the object
+//Their are two types of distrutive assignments 1)Array 2)object
+
+//Array
+
+let arr=["pavan","shubham"];
+
+let [x,y]=arr;
+console.log(x);
+
+//object
+
+let fruits={
+    name:"mango",
+    color:"yellow"
+}
+
+let {name,color}=fruits;
+console.log(name);
