5871e94e90f74d9aa50b60d63771366df72798ae
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-17 02:14:03 +0530

    .\saveTodb.js

---
+++ javascript/saveTodb.js
@@ -0,0 +1,33 @@
+// function saveToDb(data, success, failure) {
+//   let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//   if (internetSpeed > 5) {
+//     console.log("data is saved" + data);
+//     success();
+//   } else {
+//     console.log("data is not saved");
+//     failure();
+//   }
+// }
+// saveToDb(
+//   "apana collage",
+//   () => {
+//     console.log("data 1 has successfully stored");
+//     saveToDb(
+//       "succss 2",
+//       () => {
+//         console.log("data 2 has saved successfully");
+//       },
+//       () => {
+//         console.log("weak connection 2");
+//       }
+//     );
+//   },
+//   () => {
+//     console.log("weak connection 1");
+//   }
+// );
+
+function saveToDb(data) {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    
+  }
