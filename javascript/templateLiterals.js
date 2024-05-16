6ce7125dbfdce43a7c3c2b8536370775cc82ad33
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-16 20:19:50 +0530

    template literals in js

---
+++ javascript/templateLiterals.js
@@ -0,0 +1,7 @@
+//template literals is nothing but it is the format of displaying the output
+//the syntax of the sting -template literals is ${parameter} 
+//it is used inside the backticks
+
+let playername="Ms dhoni";
+
+console.log(`my favorite player is ${playername}`);
