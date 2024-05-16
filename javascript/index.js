12c24799b9afcd0def20de387e2b49f6f21de83c
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-16 20:02:43 +0530

    defference beetween let and const

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+let a=5;
+a=40;
+console.log(a); //here the value of a will be update and it becomes 40
+
+const b=5;
+b=40;
+console.log(b);//here will get the type error
+
+//we use the cosnt in the scenario where we wants to put the constant value that connot
+//changed at any cosnt in that scenario we use the cosnt
+
+//and supposingly if we are dealing with the counter varible and the varibale value is changing 
+//at that time we use let 
